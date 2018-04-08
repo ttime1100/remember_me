29ff2ecbdafd184b8a87a4df9c69c78b5d0de80a
ttime1100 <ttime1100@gmail.com>
2018-04-08 09:05:09 -0700

    database config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1090969623228-3a5r19e4kcon4mfc7gsnb0nijfdnvvi4.apps.googleusercontent.com', 'xVPcGdoClSLRyuhx1d8p2weS', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '1090969623228-k4oqh3tgubqba9tb3dj6tkvn3u2vlhqq.apps.googleusercontent.com', 'CUnq3gAloEobw_3-2xiYlbnv', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
